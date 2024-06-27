fa54bb3a6cbd1afbda642a284ff9c4b2d427170b
christian-m-castillo <christian.castillo.email@gmail.com>
2024-06-26 22:08:30 -0500

    works with different grid sizes

--- js/app.js
+++ js/app.js
@@ -1,15 +1,23 @@
 document.addEventListener('DOMContentLoaded', () => {
   const container = document.querySelector('.container');
+  const containerStyle = window.getComputedStyle(container);
+  const containerWidth = containerStyle.getPropertyValue('width');
+  const containerWidthNumericalValue = parseFloat(containerWidth);
+  const resizeSquareButton = document.querySelector('#resizeSquare');
+
+  let gridArea = 16;
+
+  resizeSquareButton.addEventListener('click', resizeGrid)
   
   generateGrid();
   armSquares();
 
   function generateGrid() {
-    for (let i = 0; i < 16; i++) {
+    container.innerHTML = '';
+    for (let i = 0; i < gridArea; i++) {
       let gridSquare = document.createElement('div');
       gridSquare.classList.add('grid-square');
       container.appendChild(gridSquare);
-      console.log('gridSquare added.')
     }
   }
 
@@ -23,4 +31,32 @@ document.addEventListener('DOMContentLoaded', () => {
   function drawColor() {
     this.style.backgroundColor = 'green';
   }
+
+  function resizeGrid() {
+    let sizeInput = prompt('Enter grid side length:'); 
+    if (sizeInput <= 10) {
+    gridArea = sizeInput * sizeInput;
+    generateGrid();
+    armSquares();
+    sizeSquares();
+    } else {
+      alert('Enter value less than or equal to 10.')
+    }
+  }
+
+  function sizeSquares() {
+    const gridSquares = document.querySelectorAll('.grid-square')
+    const gridAreaSquareRoot = Math.sqrt(gridSquares.length);
+    let squareWidth = containerWidthNumericalValue / gridAreaSquareRoot;
+    gridSquares.forEach(gridSquare => {
+      gridSquare.style.width = squareWidth * .96  + 'px';
+      console.log(containerWidthNumericalValue);
+      console.log(squareWidth);
+    });
+  }
 });
+
+// The error in this code is with the sizeSquare function.
+// The squares are rendered too narrow, and too tall.
+// In the line 'container.style.width', I may not actually be retrieving the container's width value.
+// Learn how to retrieve 
